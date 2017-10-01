80c745e66a3416b4f574e59f8e96b35feab48be4
Matt Breckon <matthew.breckon@cibc.com>
2017-10-01 17:57:28 -0400

    Handlers structure

---
+++ server/handlers/index.js
@@ -0,0 +1,5 @@
+const video = require('./video');
+
+module.exports = {
+  video
+};
---
+++ server/handlers/video.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+const path = require('path');
+
+function handler(req, res, next) {
+  const path = 'cache/Rick and Morty S03E07 - The Ricklantis Mixup [1080p].mkv';
+  const stat = fs.statSync(path);
+  const fileSize = stat.size;
+  const range = req.headers.range;
+
+  if(range) {
+    const parts = range.replace(/bytes=/, '').split('-');
+    const start = parseInt(parts[0], 10);
+    const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1;
+
+    const chunkSize = (end - start) + 1;
+    const file = fs.createReadStream(path, { start, end });
+    const head = {
+      'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+      'Accept-Ranges': 'bytes',
+      'Content-Length': chunkSize,
+      'Content-Type': 'video/mp4'
+    };
+
+    res.writeHead(206, head);
+    file.pipe(res);
+  } else {
+    const head = {
+      'Content-Length': fileSize,
+      'Content-Type': 'video/mp4'
+    };
+    res.writeHead(200, head);
+    fs.createReadStream(path).pipe(res);
+  }
+  next();
+};
+
+module.exports = { get: handler };
