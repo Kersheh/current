d7864b7e1ba5c0b7b32fdebd0ad8c9928051a93d
Matt Breckon <mattbreckon@gmail.com>
2017-10-01 23:23:54 -0400

    If else to ternary + server editorconfig

--- server/handlers/videos.js
+++ server/handlers/videos.js
@@ -3,11 +3,9 @@ const fileHelper = require('../helpers/fileHelper')
 
 router.get('/', (req, res) => {
   const videos = fileHelper.getListOfVideos();
-  if(videos.length > 0) {
-    res.status(200);
-  } else {
-    res.status(204);
-  }
+  const status = videos.length > 0 ? 200 : 204;
+
+  res.status(status);
   res.send(videos);
 });
 
