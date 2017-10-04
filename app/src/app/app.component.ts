4a08a9373f28710c83bbefc1209356f2e4feebd3
JayTea <jordantheriault@gmail.com>
2017-10-04 15:53:13 -0400

    Changed app over to using an scss precompiler

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -4,7 +4,7 @@ import { VideoService } from './shared/video.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'current';
